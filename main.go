d56a4952cb74d23f87f48db2583305ed70c6db12
Iulian Coretchi <icoretchi@iulians-MacBook-Pro-2.local>
2022-05-11 12:02:07 +0300

    locality data model

--- main.go
+++ main.go
@@ -6,3 +6,11 @@ func main() {
 	router := gin.Default()
 	router.Run()
 }
+
+type Locality struct {
+	Code            int    `json:"code"`
+	StatisticalCode int    `json:"statisticalCode"`
+	Name            string `json:"name"`
+	Status          int    `json:"status"`
+	ParentCode      int    `json:"parentCode"`
+}
