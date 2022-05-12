c8408eb8a3496a030bcd30d2235f6c2b1108f6cd
Iulian Coretchi <icoretchi@iulians-MacBook-Pro-2.local>
2022-05-12 10:40:54 +0300

    new locality endpoint

--- main.go
+++ main.go
@@ -1,16 +1,37 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type Locality struct {
+	Code            string `json:"code"`
+	StatisticalCode string `json:"statisticalCode"`
+	Name            string `json:"name"`
+	Status          string `json:"status"`
+	ParentCode      string `json:"parentCode"`
+}
+
+var localities []Locality
+
+func init() {
+	localities = make([]Locality, 0)
+}
+
+func NewLocalityHandler(c *gin.Context) {
+	var locality Locality
+	if err := c.ShouldBindJSON(&locality); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	localities = append(localities, locality)
+	c.JSON(http.StatusOK, locality)
+}
 
 func main() {
 	router := gin.Default()
+	router.POST("/localities", NewLocalityHandler)
 	router.Run()
-}
-
-type Locality struct {
-	Code            int    `json:"code"`
-	StatisticalCode int    `json:"statisticalCode"`
-	Name            string `json:"name"`
-	Status          int    `json:"status"`
-	ParentCode      int    `json:"parentCode"`
 }
